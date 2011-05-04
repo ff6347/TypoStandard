3c4df3cc31b0f2aea997c2f35c89458ae5decd6e
ontherun <fabiantheblind@googlemail.com>
2011-05-04 22:44:00 +0200

    added some links

--- some_good_links.jsx
+++ some_good_links.jsx
@@ -7,7 +7,7 @@ function main(){
 
 var theLinks = new Array();
 
-theLinks[0] = "\nhttp://www.hilfdirselbst.ch/";
+theLinks[0] = "http://www.hilfdirselbst.ch/";
 theLinks[1] = "http://www.adobe.com/products/indesign/scripting/";
 theLinks[2] = "http://www.jongware.com/idjshelp.html";
 theLinks[3] = "http://www.indesignscript.de/";
@@ -22,15 +22,20 @@ theLinks[11] = "http://www.the-moron.net/blog/?tag=javascript";
 theLinks[12] = "http://lmgtfy.com/?q=indesign+javascript";
 theLinks[13] = "http://www.mediawerk.de/mw/mw_htm_pages/head/InDesign-Menu.htm";
 theLinks[14] = "http://www.kahrel.plus.com/indesign/scriptui.html";
+theLinks[15] = "http://fabiantheblind.github.com/TypoStandard/";
+theLinks[16] = "http://fabiantheblind.github.com/theGrids/";
+theLinks[17] = "http://fabiantheblind.github.com/IDSnippets/";
+theLinks[18] = "http://www.adobe.com/devnet/scripting.html";
 
 
 // now we can loop thru the array
-var theString = "";
+var theString = "These are some fine links for starters\n";
 for(var i = 0;i < theLinks.length; i++){
 
 	theString = theString + theLinks[i] + "\n";
 	}
 	
 	alert(theString);
+	// use this in ESTK
 	// $.writeln(theString);
 }
